f8e953c7752ae6d54b50cc42d252788816e7c038
Martez Reed <martez.reed@gmail.com>
2019-03-26 10:35:51 -0500

    Update user data script to master branch

--- aws-terraform/inspec-serverless.sh
+++ aws-terraform/inspec-serverless.sh
@@ -21,14 +21,5 @@ git clone https://github.com/martezr/serverless-inspec.git
 # Change directory
 cd serverless-inspec
 
-# Change branch
-git checkout feature/native-aws
-
 # Build gem dependencies
 chmod +x build.sh && sh ./build.sh
-
-# Change directory
-cd code
-
-# Deploy stack
-sls deploy
