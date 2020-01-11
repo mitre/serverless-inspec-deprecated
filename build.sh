1fdc583aaa1715c3dc70d434359a457b6e784f7d
martezr <martez.reed@greenreedtech.com>
2020-01-10 22:18:49 -0600

    Fix build script bundle install command

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 rm -Rf ruby
 
 # Build ruby gems
-sudo docker run --rm -v $PWD:/var/gem_build -w /var/gem_build lambci/lambda:build-ruby2.5 bundle install --install-dir .
+sudo docker run --rm -v $PWD:/var/gem_build -w /var/gem_build lambci/lambda:build-ruby2.5 bundle install --path=.
 
 # Copy gems
 mkdir -p code/layer/ruby/gems/2.5.0/
